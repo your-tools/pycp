516a85c79b2608140a90b088f1bf6bb2a5656bc0
Yannick LM <yannicklm1337@gmail.com>
2010-05-01 17:15:04 +0200

    setup.py: the man pages are now automatically generated.

--- setup.py
+++ setup.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
 import pycp
+import subprocess
+
+def gen_man_pages():
+    """Simply call help2man bin/pycp """
+    res = {}
+    for f in ["pycp", "pymv"]:
+        try:
+            bin = os.path.join("bin", f)
+            p = subprocess.Popen(["help2man", "-N", bin], stdout=subprocess.PIPE)
+            out = p.communicate()[0]
+            res[f] = out
+        except OSError, e:
+            print "help2man failed. Error was", e
+            return None
+    return res
+
+man_pages = gen_man_pages()
+if not os.path.exists("doc"):
+    os.mkdir("doc")
+
+for f in ["pycp", "pymv"]:
+    man = os.path.join("doc", f + ".1")
+    f_desc = open(man, "w")
+    f_desc.write(man_pages[f])
+    f_desc.close()
+
 
 setup(name='pycp',
       version= pycp.__version__,
