8ed864d57a763178211dcbd36cd1049fb9083865
Yannick LM <yannicklm1337@gmail.com>
2010-05-02 19:01:21 +0200

    setup.py : few fixes for windows
    Removing man pages
    Adding .bat scripts

---
+++ bin/pymv.bat
@@ -0,0 +1,5 @@
+@echo off
+rem Make sure the script will be executed with the same
+rem Python it was installed for.
+set path=%~dp0;%~dp0..;%path%
+python -c "import pycp; pycp.main('move')" %*
--- setup.py
+++ setup.py
@@ -2,8 +2,15 @@
 
 from distutils.core import setup
 import os
+import sys
 import pycp
 import subprocess
+
+
+ON_WINDOWS = False
+
+if sys.platform.startswith("win"):
+    ON_WINDOWS = True
 
 def gen_man_pages():
     """Simply call help2man bin/pycp """
@@ -27,20 +34,27 @@ def gen_man_pages():
         f_desc.write(man_pages[f])
         f_desc.close()
 
-gen_man_pages()
+if not ON_WINDOWS:
+    gen_man_pages()
+    data_files = [ ("/usr/share/man/man1", ["doc/pycp.1","doc/pymv.1"]) ],
+    scripts    = ["bin/pycp", "bin/pymv"],
+
+else:
+    data_files = []
+    scripts    = ["bin/pycp.bat", "bin/pymv.bat"],
+
 
 
 setup(name='pycp',
-      version= pycp.__version__,
-      description='cp with a progressbar',
+      version          = pycp.__version__,
+      description      = 'cp with a progressbar',
       long_description = pycp.__doc__,
-      requires=['progressbar'],
-      author=pycp.__author__,
-      author_email=pycp.__author_email__,
-      url='http://sd-5791.dedibox.fr/prog/pycp.txt',
-      py_modules=['pycp'],
-      license='GPL',
-      platforms=['Linux'],
-      scripts = ["bin/pycp", "bin/pymv"],
-      data_files   =  [ ("/usr/share/man/man1", ["doc/pycp.1","doc/pymv.1"]) ]
+      requires         = ['progressbar'],
+      author           = pycp.__author__,
+      author_email     = pycp.__author_email__,
+      url              = 'http://sd-5791.dedibox.fr/prog/pycp.txt',
+      py_modules       = ['pycp'],
+      license          =' GPL',
+      scripts          = scripts,
+      data_files       = data_files,
      )
