ca1ba0e18fc5b41ae9fca82181aeaa9e1d6bebc7
Dimitri Merejkowsky <yannicklm1337@gmail.com>
2010-10-30 19:17:48 +0200

    Version 6.0

--- pycp/__init__.py
+++ pycp/__init__.py
@@ -9,7 +9,7 @@ transfer : handling transfer of files
 
 __author__ = "Yannick LM"
 __author_email__  = "yannicklm1337 AT gmail DOT com"
-__version__ = "5.0"
+__version__ = "6.0"
 
 class Options:
     pass
