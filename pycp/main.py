04dc9ed70182e1e08586d65a3d2edf2c2cc1bc5b
Dimitri Merejkowsky <d.merej@gmail.com>
2017-11-01 19:15:21 +0100

    Bump to 8.0.4

--- pycp/main.py
+++ pycp/main.py
@@ -32,7 +32,7 @@ def parse_commandline():
 
     parser = argparse.ArgumentParser(usage=usage, prog=prog_name)
 
-    parser.add_argument("-v", "--version", action="version", version="8.0.2")
+    parser.add_argument("-v", "--version", action="version", version="8.0.4")
     parser.add_argument("-i", "--interactive",
                         action="store_true",
                         dest="interactive",
